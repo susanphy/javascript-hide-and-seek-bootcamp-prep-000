45605d5307742b9ba5c3dd700e6cd29d20952002
Susan <suzutoyuki@gmail.com>
2018-02-07 06:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.querySelector('#nested .target')
 }
-index deepestChild(){
+function deepestChild(){
 
   return document.querySelector('#grand-node div div div div')
 }
